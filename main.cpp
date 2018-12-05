f247b6ab1669812c8ae05997d139ecedde111b3e
Lei Yang <lei@yangl1996.com>
2018-12-05 12:59:39 -0500

    optimize for linear regression

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ int main(int argc, char *argv[])
     int ***weight_cols;
     int ***bias_rows;
     int ***bias_cols;
-    MatrixXd ***weights;
-    MatrixXd ***biases;
+    double ***weights;
+    double ***biases;
     
     std::ifstream modelfs(argv[1]);
     if(!modelfs) {
@@ -31,8 +31,8 @@ int main(int argc, char *argv[])
     weight_cols = new int**[num_stage];
     bias_rows = new int**[num_stage];
     bias_cols = new int**[num_stage];
-    weights = new MatrixXd**[num_stage];
-    biases = new MatrixXd**[num_stage];
+    weights = new double**[num_stage];
+    biases = new double**[num_stage];
             
     for (int i = 0; i < num_stage; i++) {
         modelfs >> num_model[i];
@@ -41,8 +41,8 @@ int main(int argc, char *argv[])
         weight_cols[i] = new int*[num_model[i]];
         bias_rows[i] = new int*[num_model[i]];
         bias_cols[i] = new int*[num_model[i]];
-        weights[i] = new MatrixXd*[num_model[i]];
-        biases[i] = new MatrixXd*[num_model[i]];
+        weights[i] = new double*[num_model[i]];
+        biases[i] = new double*[num_model[i]];
         
         for (int j = 0; j < num_model[i]; j++) {
             modelfs >> num_layer[i][j];
@@ -50,29 +50,35 @@ int main(int argc, char *argv[])
             weight_cols[i][j] = new int[num_layer[i][j]];
             bias_rows[i][j] = new int[num_layer[i][j]];
             bias_cols[i][j] = new int[num_layer[i][j]];
-            weights[i][j] = new MatrixXd[num_layer[i][j]];
-            biases[i][j] = new MatrixXd[num_layer[i][j]];
+            weights[i][j] = new double[num_layer[i][j]];
+            biases[i][j] = new double[num_layer[i][j]];
             
             for (int k = 0; k < num_layer[i][j]; k++) {
                 modelfs >> weight_rows[i][j][k];
                 modelfs >> weight_cols[i][j][k];
-                weights[i][j][k].resize(weight_rows[i][j][k], weight_cols[i][j][k]);
+                if (weight_rows[i][j][k] != 1 || weight_cols[i][j][k] != 1) {
+                    std::cout << "Weights should be 1x1" << std::endl;
+                    return 1;
+                }
                 for (int r = 0; r < weight_rows[i][j][k]; r++) {
                     for (int c = 0; c < weight_cols[i][j][k]; c++) {
                         double temp;
                         modelfs >> temp;
-                        weights[i][j][k](r, c) = temp;
+                        weights[i][j][k] = temp;
                     }
                 }
                 
                 modelfs >> bias_rows[i][j][k];
                 modelfs >> bias_cols[i][j][k];
-                biases[i][j][k].resize(bias_rows[i][j][k], bias_cols[i][j][k]);
+                if (bias_rows[i][j][k] != 1 || bias_cols[i][j][k] != 1) {
+                    std::cout << "Biases should be 1x1" << std::endl;
+                    return 1;
+                }
                 for (int r = 0; r < bias_rows[i][j][k]; r++) {
                     for (int c = 0; c < bias_cols[i][j][k]; c++) {
                         double temp;
                         modelfs >> temp;
-                        biases[i][j][k](r, c) = temp;
+                        biases[i][j][k] = temp;
                     }
                 }
             }
@@ -107,21 +113,20 @@ int main(int argc, char *argv[])
     
     double err = 0.0;
     auto start_time = std::chrono::high_resolution_clock::now();
-    MatrixXd res;
+    double res;
     for (int i = 0; i < num_data; i++) {
         int model_idx = 0;
         for (int stg = 0; stg < num_stage; stg++) {
-            res = MatrixXd(1, 1);
-            res(0, 0) = double(data[i]);
+            res = double(data[i]);
             for (int lay = 0; lay < num_layer[stg][model_idx]; lay++) {
                 res = res * weights[stg][model_idx][lay] + biases[stg][model_idx][lay];
             }
-            model_idx = int(res(0, 0));
+            model_idx = int(res);
             if (model_idx >= num_model[stg+1]) {
                 model_idx = num_model[stg+1] - 1;
             }
         }
-        int final_res = int(res(0, 0));
+        int final_res = int(res);
         err += abs(final_res - pos[i]);
     }
     auto end_time = std::chrono::high_resolution_clock::now();
