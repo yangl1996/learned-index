0f5e40963837af14266f0436a295837b159850ad
Lei Yang <lei@yangl1996.com>
2018-12-05 23:45:49 -0500

    update data generation script

--- train/gendata.py
+++ train/gendata.py
@@ -6,17 +6,21 @@ parser.add_argument('--num', dest='num_data', default='100000',
                     help='number of data points of position (default: 100000)')
 parser.add_argument('--range', dest='data_range', default='300000',
                     help='range of data points (default: 300000)')
+parser.add_argument('--dist', dest='dist', default='uniform',
+                    help='distribution of data points (default: uniform)')
 
 args = parser.parse_args()
 num_data = int(args.num_data)
 data_range = int(args.data_range)
+dist = args.dist
 
 random.seed()
 
 dt = []
 
-for i in range(num_data):
-    dt.append(random.randrange(data_range))
+if dist == 'uniform':
+    for i in range(num_data):
+        dt.append(int(random.random() * data_range))
 
 dt = sorted(dt)
 
